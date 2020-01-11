d5eae5cb38655bd602007b201dad2021081c4df2
Saratkumar Puranam <saratkumar.puranam@gmail.com>
2020-01-11 16:16:52 -0500

    Version2

--- src/module1/Edurekha_Tutorials1.java
+++ src/module1/Edurekha_Tutorials1.java
@@ -3,6 +3,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 //import org.openqa.selenium.safari.SafariDriver;
 
 
@@ -12,40 +13,41 @@ WebDriver driver;
 
 
 public void invokeBrowser() {
+
 try {
-System.setProperty("webdriver.chrome.driver", "C:\\Users\\sarat\\Downloads\\Selenium_Drivers\\chromedriver.exe");
-driver = new ChromeDriver();
-
-driver.manage().deleteAllCookies();
-driver.manage().window().maximize();
-driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-
-driver.get("https://www.facebook.com");
-
-Thread.sleep(5000);
-driver.findElement(By.id("email")).sendKeys("saratkumar.puranam");
-Thread.sleep(5000);
-driver.findElement(By.id("pass")).sendKeys("Oracle9i@");
-Thread.sleep(5000);
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\sarat\\Downloads\\Selenium_Drivers\\chromedriver.exe");
+	driver = new ChromeDriver();
+	
+	driver.manage().deleteAllCookies();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+	
+	driver.get("https://www.facebook.com");
+	Thread.sleep(5000);
+	driver.findElement(By.id("u_0_f")).sendKeys("SaratKumar");
+	Thread.sleep(5000);
+	driver.findElement(By.name("lastname")).sendKeys("Puranam");
+	driver.findElement(By.id("u_0_k")).sendKeys("saratkumar.puranam@gmail.com");
+	Thread.sleep(10000);
+	driver.findElement(By.id("u_0_n")).sendKeys("saratkumar.puranam@gmail.com");
+	Thread.sleep(5000);
+	driver.findElement(By.id("u_0_p")).sendKeys("Welcome1@");
+	Select months = new Select (driver.findElement(By.id("month")));
+	months.selectByIndex(12);
+	Select days = new Select (driver.findElement(By.id("day")));
+	days.selectByVisibleText("20");
+	Select years = new Select (driver.findElement(By.id("year")));
+	years.selectByVisibleText("1976");
+	driver.findElement(By.id("u_0_a")).click();
 } catch (InterruptedException e) {
-// TODO Auto-generated catch block
-e.printStackTrace();
+	// TODO Auto-generated catch block
+	e.printStackTrace();
 }
 }
-
-
-/*public void SearchLogin() {
-
-
-//driver.findElement(By.id("email")).click();
-driver.findElement(By.id("u_0_f")).sendKeys("saratkumar.puranam");
-}
-*/
-
 public static void main(String[] args) {
 
-	Edurekha_Tutorials1 myClass = new Edurekha_Tutorials1();
+Edurekha_Tutorials1 myClass = new Edurekha_Tutorials1();
 myClass.invokeBrowser();
 
 }
